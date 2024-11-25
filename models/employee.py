963ae64a1c5a2f6b42d3fc3d55820590be57a0b5
binu <binu.christin@faithinfotech.in>
2024-11-25 15:40:29 +0530

    created a project

---
+++ models/employee.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self, employeeName, age):
+        self.employeeName = employeeName
+        self.age = age
+    def display(self):
+        print(f"the employeename is 
+              {self.employeeName}")
+    
